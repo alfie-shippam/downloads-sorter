2adb33b6abfa1fa89181c0200928e02f336b70df
alfie <alfshippam@gmail.com>
2024-07-26 16:51:00 +0100

    Updated to run continuously

--- file_sorter.py
+++ file_sorter.py
@@ -1,12 +1,9 @@
+import time
 import os
 import shutil
 
-# Define the path to the downloads folder
 downloads_path = os.path.join(os.path.expanduser('~'), 'Downloads')
 
-print(f"Downloads path: {downloads_path}")
-
-# Define file type categories
 file_types = {
     'PDFs': ['.pdf'],
     'Data': ['.xlsx', '.csv', '.gsheet'],
@@ -18,30 +15,35 @@ file_types = {
     'Others': []
 }
 
-# Create subfolders if they don't exist
-for folder in file_types.keys():
-    folder_path = os.path.join(downloads_path, folder)
-    os.makedirs(folder_path, exist_ok=True)
+def sort_files():
+    for folder in file_types.keys():
+        folder_path = os.path.join(downloads_path, folder)
+        os.makedirs(folder_path, exist_ok=True)
 
-# Scan the downloads directory and move files to appropriate folders
-for item in os.listdir(downloads_path):
-    item_path = os.path.join(downloads_path, item)
-    
-    if os.path.isfile(item_path):
-        _, file_extension = os.path.splitext(item_path)
-        file_extension = file_extension.lower()
+    for item in os.listdir(downloads_path):
+        item_path = os.path.join(downloads_path, item)
         
-        moved = False
-        for folder, extensions in file_types.items():
-            if file_extension in extensions:
-                target_folder = os.path.join(downloads_path, folder)
-                shutil.move(item_path, os.path.join(target_folder, item))
-                moved = True
-                break
-        
-        # If file type is not categorized, move to 'Others'
-        if not moved:
-            others_folder_path = os.path.join(downloads_path, 'Others')
-            shutil.move(item_path, os.path.join(others_folder_path, item))
+        if os.path.isfile(item_path):
+            _, file_extension = os.path.splitext(item_path)
+            file_extension = file_extension.lower()
+            
+            moved = False
+            for folder, extensions in file_types.items():
+                if file_extension in extensions:
+                    target_folder = os.path.join(downloads_path, folder)
+                    shutil.move(item_path, os.path.join(target_folder, item))
+                    moved = True
+                    break
+            
+            if not moved:
+                others_folder_path = os.path.join(downloads_path, 'Others')
+                shutil.move(item_path, os.path.join(others_folder_path, item))
 
-print("Files sorted.")
+def main():
+    while True:
+        sort_files()
+        print("Files sorted.")
+        time.sleep(30)
+
+if __name__ == "__main__":
+    main()
